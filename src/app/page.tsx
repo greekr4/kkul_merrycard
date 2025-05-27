0d9e7771562da4ef528ac1ca77d2d099aced5a85
TKman <102001532+greekr4@users.noreply.github.com>
2025-05-27 16:26:26 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,7 @@ import ClassicTemplate from "@/component/templates/ClassicTemplate";
 export default function Home() {
   return (
     <>
-      <ClassicTemplate
-        groom="John Doe"
-        bride="Jane Doe"
-        date="2026-09-19 11:00:00"
-        time="10:00"
-        place="123 Main St, Anytown, USA"
-      />
+      <ClassicTemplate date="2025-08-17 11:00:00" />
     </>
   );
 }
